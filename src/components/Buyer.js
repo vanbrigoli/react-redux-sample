75a4a3c836a96f90f7b021bab8665ed84876c86b
jezznar <jessnarsinues@gmail.com>
2017-08-24 03:25:22 +0800

    Hardcoded placeholders/mockups on food menu

--- src/components/Buyer.js
+++ src/components/Buyer.js
@@ -30,31 +30,52 @@ class Buyer extends Component {
                             <fieldset className="tray">
                                 <legend>Food Menu</legend>
                                 {/*Hardcoded mockups | Placeholders*/}
-                                <table className="table table-bordered table-responsive">
-                                    <thead>
+                                <div className="card" >
+                                    <table className="table table-responsive">
+                                        <tbody>
                                         <tr>
-                                            <th>Item</th>
-                                            <th>Price</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td> item 1 </td>
-                                            <td> 500 </td>
-                                            <td> 
-                                                <button className="btn btn-success"> Add to Tray </button> 
+                                            <td>
+                                                <img className="card-img-top" src="https://a8g4i9g5y.ssl.hwcdn.net/files/styles/food_image/public/image/7b/94/107805-filipino-pork-adobo.png" alt="Card image cap"></img>
+                                            </td>
+                                            <td>
+                                                <div className="card-block">
+                                                    <td>
+                                                    <h4 className="card-title"><strong>Adobong Kesarapsarap</strong></h4>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" className="btn btn-primary">Add</a>
+                                                    </td>
+                                                    <h3>Php 90.00</h3>
+                                                    <p className="card-text">Better than any adobo in world, Rovelle's adobo will surely make you go loko!</p>
+                                                </div>
                                             </td>
                                         </tr>
+                                        </tbody>
+                                    </table>
+                                </div>   
+                                <div className="card" >
+                                    <table className="table table-responsive">
+                                        <tbody>
                                         <tr>
-                                            <td> item 2 </td>
-                                            <td> 500 </td>
-                                            <td> 
-                                                <button className="btn btn-success"> Add to Tray </button>
+                                            <td>
+                                                <img className="card-img-top" src="https://scm-assets.constant.co/scm/unilever/e9dc924f238fa6cc29465942875fe8f0/77d08f50-3ccc-4432-a86d-4dcfdd3d7cd4.jpg" alt="Card image cap"></img>
+                                            </td>
+                                            <td>
+                                                <div className="card-block">
+                                                    <td>
+                                                    <h4 className="card-title"><strong>Sinigang Na Keasimsim</strong></h4>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" className="btn btn-primary">Add</a>
+                                                    </td>
+                                                    <h3>Php 120.00</h3>
+                                                    <p className="card-text">Rovelle's Sinigang, will make you go tigang with its sour taste and distinct flavor.</p>
+                                                </div>
                                             </td>
                                         </tr>
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>                                                              
                             </fieldset>
                         </div> 
                         <div className="PanelCol col-sm-6">
