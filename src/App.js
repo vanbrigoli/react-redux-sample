44c42e2f0bdf34998debd70f110d526fcf41a0ac
Ninja <jessnarsinues@gmail.com>
2017-08-23 21:48:32 +0800

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master'"
    This reverts commit 6a2d96bae8967415e97e97f0a524cad3fb3edb1b, reversing
    changes made to 9103ba9ccaef29d8217d974dc4dd9104b2aee755.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { browserHistory } from 'react-router';
 import './App.css';
 
 class App extends Component {
+    goToVendorPage(event) {
+        event.preventDefault();
+        browserHistory.push('/vendor');
+    }
+
+    goToBuyerPage(event) {
+        event.preventDefault();
+        browserHistory.push('/buyer');
+    }
+
     render() {
         return (
             <div className="container">
-                <div className="jumbotron App-center">
+                <div className="jumbotron">
                     <h1>Welcome to E-bili</h1>
-                    <p>Kain tayo ti!</p>
+                    <p>Try one now</p>
                 </div>
-                <div className="App-center">
-                    <Link to="/vendor" type="button" className="btn btn-primary btn-lg">Vendor</Link>
-                    <Link to="/buyer" type="button" style={{marginLeft: 10}} className="btn btn-success btn-lg">Buyer</Link>
+                <div className="App-btn-center">
+                    <button type="button" className="btn btn-primary btn-lg" onClick={this.goToVendorPage.bind(this)}>Vendor</button>
+                    <button type="button" className="btn btn-success btn-lg" onClick={this.goToBuyerPage.bind(this)}>Buyer</button>
                 </div>
             </div>
         )
