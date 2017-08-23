71c8a1d3c62813238cfabc7c3e212bdfb298fc30
jezznar <jessnarsinues@gmail.com>
2017-08-23 22:04:31 +0800

    Buyer Component: Added alert placeholder.

--- src/components/Buyer.js
+++ src/components/Buyer.js
@@ -17,15 +17,27 @@ class Buyer extends Component {
                 <div className="Buyer-container">
                     <div className="row">
                         <div className="ItemCol col-sm-8">
-                            <AddItem/>
+                            <fieldset className="tray">
+                                <legend>Food Menu</legend>
+                                <AddItem/>
+                            </fieldset>                            
                         </div> 
                         <div className="TrayCol col-sm-4">
                             <fieldset className="tray">
-                                <legend>Tray:</legend>
+                                <legend>Tray</legend>
                                 {itemlist}
                             </fieldset>
                         </div>
                     </div>
+                </div>
+                <div className="alert alert-success">
+                    <button type="button" className="close" data-dismiss="alert" aria-hidden="true">
+                        ×</button>
+                    <span className="glyphicon glyphicon-info-sign"></span> <strong>Info Message</strong>
+                    <hr className="message-inner-separator"/>
+                        <p>
+                            Your order ID: <strong>00001</strong> has been placed.<br/>Please wait for your queue.
+                        </p>
                 </div>
             </div>
         );        
