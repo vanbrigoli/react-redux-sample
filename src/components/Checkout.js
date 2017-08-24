d84717b3a9b1153747b751fc013b570333b06ca7
amanuel <albert.manuel@nokia.com>
2017-08-24 13:04:41 +0800

    Add order list component when user submits his order

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
+import UserOrderList from './UserOrderList';
 
 const Checkout = () => {
     return (
         <div>
             <Link to="/" className="btn btn-success">Home</Link>
+            <div className="container">
+              <UserOrderList/>
+            </div>
         </div>
     )
 };
