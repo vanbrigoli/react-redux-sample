950566cbe21e376145d98d315df44a8daf4e7235
jezznar <jessnarsinues@gmail.com>
2017-08-25 12:58:44 +0800

    Follow up Navbar fix: hide vendor-view navs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,19 @@ class Navbar extends Component {
     render() {
         const vendorClass = browserHistory.getCurrentLocation().pathname === '/vendor' ? 'active': '';
         const orderListClass = browserHistory.getCurrentLocation().pathname === '/order-list' ? 'active': '';
-        const hiddenClass = browserHistory.getCurrentLocation().pathname === '/buyer' || '/checkout' ? "nav navbar-nav hidden": "nav navbar-nav"
+        var hiddenClass = "";
+        if (browserHistory.getCurrentLocation().pathname === '/checkout' || '/buyer')
+            hiddenClass = "nav navbar-nav hidden";
+        else
+            hiddenClass = "nav navbar-nav";
+
         return (
             <nav className="navbar navbar-default navbar-fixed-top">
                 <div className="container">
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className={hiddenClass}>
-                            <li className={vendorClass}><Link to="/vendor">{vendorClass}Vendor Window</Link></li>
-                            <li className={orderListClass}><Link to="/order-list">{orderListClass}Order List</Link></li>
+                            <li className={vendorClass}><Link to="/vendor">Vendor Window</Link></li>
+                            <li className={orderListClass}><Link to="/order-list">Order List</Link></li>
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
                             <li><Link to="/">Home</Link></li>
